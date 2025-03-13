17708fd718604774b70e7589f6a8dcae152b9b22
marrazki <martin_leoz@euneiz.com>
2025-03-13 20:11:24 +0100

    BillarAsset

--- Assets/Scripts/Bolas.cs
+++ Assets/Scripts/Bolas.cs
@@ -1,9 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Bolas : MonoBehaviour
 {
-    public List <Bolas> bolas;
-
+    public int numero;
+    public bool tipo;
 }
