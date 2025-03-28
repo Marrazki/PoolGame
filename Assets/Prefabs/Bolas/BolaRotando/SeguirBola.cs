0403cec1f1b9bf7b231f1c87ef849021d00ac5e5
marrazki <martin_leoz@euneiz.com>
2025-03-28 17:15:12 +0100

    Prefabs bolas rotando

---
+++ Assets/Prefabs/Bolas/BolaRotando/SeguirBola.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class SeguirBola : MonoBehaviour
+{
+    public GameObject bola;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = bola.transform.position;
+    }
+}
